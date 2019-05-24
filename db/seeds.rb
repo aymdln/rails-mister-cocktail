c7fe532f61e3b50149b4cf1955fa7902bfd4ed3c
Aymeric DHALLUIN <aymeric.dhalluin@gmail.com>
2019-05-24 17:54:00 +0200

    Finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,20 +25,22 @@ end
 puts " [OK]".colorize(:green)
 puts "Finish ... add #{Ingredient.count} ingredients"
 
-nmb = 6
-print "Add #{nmb} cocktails"
-nmb.times do 
-    Cocktail.create!(name: Faker::Coffee.blend_name)
-end
-puts " [OK]".colorize(:green)
+# nmb = 6
+# print "Add #{nmb} cocktails"
+# nmb.times do 
+#     Cocktail.create!(
+#         name: Faker::Coffee.blend_name
+#         )
+# end
+# puts " [OK]".colorize(:green)
 
-print "Add doses"
-i = 0
-100.times do
-    Dose.create!(
-    description: "#{rand(0..100)}cl",
-    cocktail_id: rand(1..Cocktail.count),
-    ingredient_id: i += 1
-    )
-end
-puts " [OK]".colorize(:green)
+# print "Add doses"
+# i = 0
+# 100.times do
+#     Dose.create!(
+#     description: "#{rand(0..100)}cl",
+#     cocktail_id: rand(1..Cocktail.count),
+#     ingredient_id: i += 1
+#     )
+# end
+# puts " [OK]".colorize(:green)
