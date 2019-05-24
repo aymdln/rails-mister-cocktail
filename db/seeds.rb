f9d10d2942eea88c4f82abaf6d19e695488d193c
Aymeric DHALLUIN <aymeric.dhalluin@gmail.com>
2019-05-24 16:10:36 +0200

    J'ai fait plein de truc

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # require 'open-uri'
 # require 'json'
+
 print "Clean base"
 Ingredient.destroy_all
 puts " [OK]".colorize(:green)
+
 print "Get to api"
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredient_serialized = open(url).read
 ingredient = JSON.parse(ingredient_serialized)
-
 puts " [OK]".colorize(:green)
 
 print "Add Ingredients"
@@ -24,12 +25,20 @@ end
 puts " [OK]".colorize(:green)
 puts "Finish ... add #{Ingredient.count} ingredients"
 
-nmb = 5
-
+nmb = 6
 print "Add #{nmb} cocktails"
-
 nmb.times do 
     Cocktail.create!(name: Faker::Coffee.blend_name)
 end
+puts " [OK]".colorize(:green)
 
+print "Add doses"
+i = 0
+100.times do
+    Dose.create!(
+    description: "#{rand(0..100)}cl",
+    cocktail_id: rand(1..Cocktail.count),
+    ingredient_id: i += 1
+    )
+end
 puts " [OK]".colorize(:green)
